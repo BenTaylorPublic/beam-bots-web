7be2f8144fcfbfa832a85842da328a7a05d519d7
Ben <Benjaminjacktaylor@hotmail.com>
2021-04-23 06:50:19 +1000

    Using socket io client npm package instead

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,9 +1,15 @@
 import {ExampleService} from "../../shared/example-service";
+import {io} from "socket.io-client";
 
 class IndexView {
 
     public static initialize(): void {
         ExampleService.logHelloWorld();
+
+        const socket = io("http://localhost:8181");
+        socket.on("connect", () => {
+            console.log(socket.id);
+        });
     }
 }
 
