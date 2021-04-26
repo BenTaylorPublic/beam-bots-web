ae7ddc7e32e61156006548f9b7fd4a1a27b4a30d
Ben <Benjaminjacktaylor@hotmail.com>
2021-04-26 21:55:38 +1000

    Drawing the circle in the ice scene

--- src/pages/index/game/scenes/minigame-ice-circle.ts
+++ src/pages/index/game/scenes/minigame-ice-circle.ts
@@ -14,12 +14,16 @@ export class MinigameIceCircle extends IGameScene {
     private acceleration: number;
     private players: PlayerInfoMgIceCircle[];
     private localPlayerRadius: number;
+    private circleCenter: Point2D;
+    private circleRadius: number;
 
     constructor(setMinigameIceCircleScene: SetMinigameIceCircleScene) {
         super();
         this.context.font = "50px monospace";
         this.acceleration = setMinigameIceCircleScene.acceleration;
         this.players = setMinigameIceCircleScene.players;
+        this.circleCenter = HelperService.convertGameLogicCoordsToLocalCoords(setMinigameIceCircleScene.circleCenter);
+        this.circleRadius = HelperService.convertGameLogicPixelsToLocalPixels(setMinigameIceCircleScene.circleRadius);
         this.localPlayerRadius = HelperService.convertGameLogicPixelsToLocalPixels(setMinigameIceCircleScene.playerHitBoxRadius);
         this.background.style.backgroundImage = "url('/assets/ice_circle_bg.png')";
     }
@@ -35,6 +39,13 @@ export class MinigameIceCircle extends IGameScene {
     }
 
     protected loop(): void {
+        this.context.fillStyle = "transparent";
+        this.context.beginPath();
+        this.context.arc(this.circleCenter.x, this.circleCenter.y, this.circleRadius, 0, 2 * Math.PI);
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = "#FF0000";
+        this.context.stroke();
+
         for (let i: number = 0; i < this.players.length; i++) {
             this.context.fillStyle = HelperService.convertColorToHexcode(this.players[i].player.color);
             this.context.beginPath();
